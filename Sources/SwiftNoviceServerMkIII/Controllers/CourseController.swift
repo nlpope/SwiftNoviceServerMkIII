ff889473426d8a0629f35d186597b15d3f5ebd3a
Noah Pope <npope@tutanota.com>
2025-09-07 02:08:57 -0500

    adding comments

--- Sources/SwiftNoviceServerMkIII/Controllers/CourseController.swift
+++ Sources/SwiftNoviceServerMkIII/Controllers/CourseController.swift
@@ -6,12 +6,20 @@ import Fluent
 import Vapor
 
 // STEP 3: THIS CONTROLLER HAS THE ABILITY TO READ ALL THE DIFFERENT COURSES VIA THE '.../courses' ROUTE
-// BUT, VAPOR IS ONLY RUNNING THE API, NOT THE DATABASE IT DEPENDS ON, SO IN YOUR TERMINAL, GO IN THIS SERVER'S FILE > HIT 'CMD + T' > 'docker compose up db' (see docker-compose.yml file in this project) > BE SURE THE DOCKER DESKTOP APP IS OPEN OR IT WILL FAIL AGAIN
-// ALL THIS IS DOING IS CREATING THE DATABASE FOR YOU WITHIN A DOCKER CONTAINER SO THE VAPOR API HAS SOMETHING TO SHOVE/PULL TO/FROM
-// ONCE IT'S RUNNING, GO INTO POSTMAN TO CONFIRM THE ROUTES WORK (you can liken postman to the actual user facing app: postman(user-facing app || window into API) > Vapor SwiftNoviceServerMkIII API (server) > docker via terminal (database) > azure data studio (optional window into database))
+/**
+ Steps & Analogies:
+ 1. turn on docker (database) via terminal
+ > terminal > go to server file > 'cmd + t' for new tab > type 'docker compose up db' (see docker.yml file) > be sure the desktop app is open or this will fail
+ > azure data studio (API-agnostic db window) is a window for this - open it & make a new connection w info from 'configure' file
+ >> create a new connection > type postgresSQL > servername localhost (see configure file) > usrname & password = vapor_username & vapor_password (see configure file) > database = vapor_database (see configure file)
+ 
+ 2. turning on docker db (something for the localhost/url/port/server to pull from) allows you to turn on the main swift vapor (API/server) app
+ > run the project
+ 
+ 3. open postman (window into API/server || stand in for user-facing app) to ensure vapor app/port/url is returning '200 OK' + any expected '/route' outputs
+ */
+
 #warning("one last possible error @ this point = 'the port/address is already in use'. if this happens, in terminal type 'lsof -i:8080' to reveal all the ports in use. Then type 'kill -9 [PID # revealed from last command]' just omit the brackets")
-// NOW OPEN AZURE DATA STUDIO, this is just a window into our database that doesn't require an API
-// in azure create a new connection > type postgresSQL > servername localhost (see configure file) > usrname & password = vapor_username & vapor_password (see configure file) > database = vapor_database (see configure file)
 
 struct CourseController: RouteCollection
 {
