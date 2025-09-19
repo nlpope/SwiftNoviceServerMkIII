46c465199b1f40902ed72b038ad6756e0ada9b02
Noah Pope <npope@tutanota.com>
2025-09-18 20:03:20 -0500

    adding constants utility and fleshing out createCourse method in routes

---
+++ Sources/SwiftNoviceServerMkIII/Utilities/Constants+Utils.swift
@@ -0,0 +1,15 @@
+//  File: Constants+Utils.swift
+//  Project: SwiftNoviceServerMkIII
+//  Created by: Noah Pope on 9/18/25.
+
+import Foundation
+
+enum imageKeys
+{
+    static let paulHudson = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.r9OsmuPFWaYXAuycMaJaRgHaHa%3Fpid%3DApi&f=1&ipt=05ee970f7cb838b237ca7e881abcb6bdac6140164b8dc1d8a69d704370592dd0&ipo=images"
+}
+
+enum BioKeys
+{
+    static let paulHudson = "jerflibbasclepaaaaaaaaaaguiudhfajkshfajlsdhfalsjdhfasjdlfha;klsdjfaks;ldfj;alksdjf;alksdfja;lsdfjka;lsdkfja;lsdfjkas;dlfja;sdlfj"
+}
--- Sources/SwiftNoviceServerMkIII/routes.swift
+++ Sources/SwiftNoviceServerMkIII/routes.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Fluent
 import Vapor
 
@@ -22,10 +23,18 @@ func routes(_ app: Application) throws
     app.get("getCourses") { req async -> [Course] in
         var courses = [Course]()
         
+        courses += createCourse(named: "HWS", taughtBy: "Paul Hudson", atIndex: 1, url: "https://www.hackingwithswift.com/", imageUrl: "", bio: BioKeys.paulHudson)
+        
         return courses
     }
     
     // END Swift's vapor docs
     
     try app.register(collection: CourseController())
+}
+
+
+func createCourse(named name: String, taughtBy instructor: String, atIndex index: Int, url: String, imageUrl: String, bio: String) -> Course
+{
+    return Course(id: UUID(), itemIndex: index, name: name, instructor: instructor, bio: bio, avatarUrl: imageUrl, isBookmarked: false)
 }
