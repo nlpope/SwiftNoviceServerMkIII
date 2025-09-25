4653ec22a6dfc93106258991b9983d3a2a33edb0
Noah Pope <npope@tutanota.com>
2025-09-25 09:19:01 -0500

    adding rest of courses to courseCreator()

--- Sources/SwiftNoviceServerMkIII/Utilities/CourseCreator+Utils.swift
+++ Sources/SwiftNoviceServerMkIII/Utilities/CourseCreator+Utils.swift
@@ -15,11 +15,26 @@ class CourseCreator
     func createCourses() -> [Course]
     {
         var courses = [Course]()
-        let paul = createCourse(named: "HWS", taughtBy: "Paul Hudson", atIndex: 1, url: CourseUrls.paulHudson, avatarUrl: AvatarUrls.paulHudson, bio: BioKeys.paulHudson)
         
-        let git = createCourse(named: "Git beginner", taughtBy: "Will Smythe", atIndex: 2, url: CourseUrls.paulHudson, avatarUrl: AvatarUrls.paulHudson, bio: BioKeys.paulHudson)
         
-        courses += [paul]
+        let git = createCourse(named: "Git beginner", taughtBy: "Will Smythe", atIndex: 1, url: CourseUrls.git, avatarUrl: AvatarUrls.git, bio: BioKeys.git)
+        
+        let playgrounds1 = createCourse(named: "Swift Playgrounds 1", taughtBy: "Self guided", atIndex: 2, url: CourseUrls.playgrounds1, avatarUrl: AvatarUrls.playgrounds1, bio: BioKeys.playgrounds1)
+        
+        let playgrounds2 = createCourse(named: "Swift Playgrounds 2", taughtBy: "Self guided", atIndex: 3, url: CourseUrls.playgrounds2, avatarUrl: AvatarUrls.playgrounds2, bio: BioKeys.playgrounds2)
+        
+        let seanAllenBeginner = createCourse(named: "Swift Beginner Course", taughtBy: "Sean Allen", atIndex: 4, url: CourseUrls.seanAllenBeginner, avatarUrl: AvatarUrls.seanAllenBeginner, bio: BioKeys.seanAllenBeginner)
+        
+        let seanAllenTakeHome = createCourse(named: "Dev Interview Prep - Take Home Project", taughtBy: "Sean Allen", atIndex: 5, url: CourseUrls.seanAllenTakeHome, avatarUrl: AvatarUrls.seanAllenTakeHome, bio: BioKeys.seanAllenTakeHome)
+        
+        let stanford = createCourse(named: "Stanford CS193P iOS course - Debugging and Xcode tips & tricks", taughtBy: "Jason Riggs", atIndex: 6, url: CourseUrls.stanford, avatarUrl: AvatarUrls.stanford, bio: BioKeys.stanford)
+        
+        let lynch = createCourse(named: "Mastering XCode", taughtBy: "Stewart Lynch", atIndex: 7, url: CourseUrls.lynch, avatarUrl: AvatarUrls.lynch, bio: BioKeys.lynch)
+        
+        let paul = createCourse(named: "HWS", taughtBy: "Paul Hudson", atIndex: 8, url: CourseUrls.paulHudson, avatarUrl: AvatarUrls.paulHudson, bio: BioKeys.paulHudson)
+        
+        courses += [git, playgrounds1, playgrounds2, seanAllenBeginner, seanAllenTakeHome, stanford, lynch, paul]
+        
         return courses
     }
 
