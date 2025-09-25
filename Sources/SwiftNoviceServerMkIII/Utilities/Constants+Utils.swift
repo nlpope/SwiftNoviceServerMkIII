de33bfa06e363ff373f5f765bfb187f9391c9b34
Noah Pope <npope@tutanota.com>
2025-09-25 08:53:29 -0500

    adding course constants (bios, avatar urls, etc.) to utils

--- Sources/SwiftNoviceServerMkIII/Utilities/Constants+Utils.swift
+++ Sources/SwiftNoviceServerMkIII/Utilities/Constants+Utils.swift
@@ -6,7 +6,6 @@ import Foundation
 
 enum CourseUrls
 {
-    static let paulHudson = "https://www.hackingwithswift.com/"
     static let git = "https://learngitbranching.js.org/"
     static let playgrounds1 = "https://app.alludolearning.com/m/activities/147676-swift-playgrounds-getting-started-with-learn-to-code-1-commands"
     static let playgrounds2 = "https://app.alludolearning.com/m/activities/147676-swift-playgrounds-getting-started-with-learn-to-code-1-commands"
@@ -14,14 +13,29 @@ enum CourseUrls
     static let seanAllenTakeHome = "https://www.youtube.com/watch?v=JzngncpZLuw&ab_channel=SeanAllen"
     static let stanford = "https://www.youtube.com/watch?v=CRxHhx_pubY&list=PL3d_SFOiG7_8ofjyKzX6Nl1wZehbdiZC_&index=3&ab_channel=CS193P"
     static let lynch = "https://www.youtube.com/watch?v=R2ptqaBMan4&list=PLBn01m5Vbs4CUCcA3fqvsRtx153akAJU1&index=1&ab_channel=StewartLynch"
+    static let paulHudson = "https://www.hackingwithswift.com/"
 }
 
 enum AvatarUrls
 {
+    static let git = "https://www.javiertordable.com/images/learn-git3.png"
+    static let playgrounds1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXFGMHPXBCykZN35aX3Oetve4cHKtZ0sZxaQ&s"
+    static let playgrounds2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbGf3_LSit8Rqdoe09lzaOXe-5ivF_WY1jzA&s"
+    static let seanAllenBeginner = "https://avatars.githubusercontent.com/u/10645516?v=4"
+    static let seanAllenTakeHome = "https://avatars.githubusercontent.com/u/10645516?v=4"
+    static let stanford = "https://miro.medium.com/v2/resize:fit:1110/1*SM3MvxLn6D3yigl-RrJP7w@2x.jpeg"
+    static let lynch = "https://avatars.githubusercontent.com/u/2940924?v=4"
     static let paulHudson = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.r9OsmuPFWaYXAuycMaJaRgHaHa%3Fpid%3DApi&f=1&ipt=05ee970f7cb838b237ca7e881abcb6bdac6140164b8dc1d8a69d704370592dd0&ipo=images"
 }
 
 enum BioKeys
 {
-    static let paulHudson = "jerflibbasclepaaaaaaaaaaguiudhfajkshfajlsdhfalsjdhfasjdlfha;klsdjfaks;ldfj;alksdjf;alksdfja;lsdfjka;lsdkfja;lsdfjkas;dlfja;sdlfj"
+    static let git = "Interested in learning Git? Well you've come to the right place! 'Learn Git Branching' is the most visual and interactive way to learn Git on the web; you'll be challenged with exciting levels, given step-by-step demonstrations of powerful features, and maybe even have a bit of fun along the way."
+    static let playgrounds1 = "Learn to code with Swift Playgrounds. Swift Playgrounds is a revolutionary app for iPad and Mac that helps you learn to code and build apps using Swift, the same powerful language used to create world-class apps for the App Store."
+    static let playgrounds2 = "Learn to code with Swift Playgrounds. Swift Playgrounds is a revolutionary app for iPad and Mac that helps you learn to code and build apps using Swift, the same powerful language used to create world-class apps for the App Store."
+    static let seanAllenBeginner = "A course in Swift basics for absolute beginners."
+    static let seanAllenTakeHome = "In this course I walk you through a mock take home project that is very common in the iOS dev job interview process. We start from the project brief, design, and we build the entire thing together so you can practice for the real thing. This project fetches a list of followers from a GitHub username via a network call and allows the user to set and persist a favorite GitHub user."
+    static let stanford = "Jason Riggs talks about debugging and shares Xcode tips and tricks."
+    static let lynch = "This is the first of a 3 part series on Taming the beast known as Xcode. The series will be broken into 3 distinct sections.  In this first video, I will go over what has confused me a lot;  window and document tabs along with editors and how these all fit together.  I'll provide you with some tips for keeping your hands on the keyboard and still be able to navigate your way around."
+    static let paulHudson = "The Hacking with Swift tutorial series is designed to make it easy for beginners to get started coding for iPad and iPhone using the Swift programming language. Hacking with Swift’s teaching style is extremely hands-on, which means nearly everything you learn from me will have a direct, practical application to something we're working on. That way, your brain can see exactly why a certain technique is helpful and you can start using it straight away."
 }
